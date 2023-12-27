86f613b5c254a43cbc2d80c08483e5f5ffbde37b
Alvin Cris Tabontabon <me@acltabontabon.com>
2023-12-28 00:44:28 +0800

    Base empty project

---
+++ src/test/java/com/acltabontabon/kwhcalculator/KwhCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acltabontabon.kwhcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwhCalculatorApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
